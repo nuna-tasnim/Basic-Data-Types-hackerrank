f526dadde1aba09f9b2f5b596182dbafffa9f354
nuna-tasnim <56688040+nuna-tasnim@users.noreply.github.com>
2019-12-03 21:21:15 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include <iomanip>
+using namespace std;
+
+int main() {
+   int a; long b; char c; double d;float f;
+   cin>>a;
+   cin>>b;
+   cin>>c;
+
+   cin>>f;
+   cin>>d;
+   cout<<a<<endl;
+   cout<<b<<endl;
+   cout<<c<<endl;
+
+cout<<fixed<<setprecision(3)<<f<<endl;
+
+cout<<fixed<<setprecision(9)<<d<<endl;
+
+
+
+    return 0;
+}
+
